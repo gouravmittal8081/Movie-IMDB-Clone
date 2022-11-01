aa6abbfd5e502bd645d7f08aaa272074ea680658
gourav mittal <107996221+gouravmittal8081@users.noreply.github.com>
2022-11-01 21:09:56 +0530

    Update card.js

--- src/component/card/card.js
+++ src/component/card/card.js
@@ -1,14 +1,17 @@
 // npm i react-loading-skeleton => hamne ye install kya haikyuki card me hame kuch alag hi effect add krna hai
 
 import React, {useEffect, useState} from "react"
+//jab ham kisi page ko refresh karenge to ye kuch time lega load hone ke liye isiliye hamne yaha ek pachage import kiya SKELETON
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton"
 import "./card.css"
 import { Link } from "react-router-dom"
 
 const Cards = ({movie}) => {
 
+
     const [isLoading, setIsLoading] = useState(true)
 
+//yha ye usLoading matlb jo car load ho rha hai skeleton se usko 1.5 second ke bad false kar dega
     useEffect(() => {
         setTimeout(() => {
             setIsLoading(false)
@@ -17,6 +20,7 @@ const Cards = ({movie}) => {
 
     return <>
     {
+    //agar bo card load ho rha hai to ye condition run hogi
         isLoading
         ?
         <div className="cards">
@@ -25,9 +29,12 @@ const Cards = ({movie}) => {
             </SkeletonTheme>
         </div>
         :
+        //jese hi ham car pr click karenge to ham uske ID pr pahuch jayenge
         <Link to={`/movie/${movie.id}`} style={{textDecoration:"none", color:"white"}}>
             <div className="cards">
+            //hamne yha cards_img me pehle uski API lagai then agar vo condition true hogi to movie ke path ko show karega
                 <img className="cards__img" src={`https://image.tmdb.org/t/p/original${movie?movie.poster_path:""}`} />
+                
                 <div className="cards__overlay">
                     <div className="card__title">{movie?movie.original_title:""}</div>
                     <div className="card__runtime">
