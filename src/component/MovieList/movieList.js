460f315818e3d3ff4deef687fccfbccffd16c111
gourav mittal <107996221+gouravmittal8081@users.noreply.github.com>
2022-11-01 21:28:21 +0530

    Update movieList.js

--- src/component/MovieList/movieList.js
+++ src/component/MovieList/movieList.js
@@ -6,18 +6,22 @@ import Cards from "../card/card"
 const MovieList = () => {
     
     const [movieList, setMovieList] = useState([])
+ //hame ye params type batayega ki vo kis type ka hai 
     const {type} = useParams()
 
     useEffect(() => {
         getData()
     }, [])
 
+//Ab agar param ko type me change milega to ye is getData ko call kara dega
     useEffect(() => {
         getData()
     }, [type])
 
+
     const getData = () => {
-        fetch(`https://api.themoviedb.org/3/movie/${type ? type : "popular"}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
+    //yha agar ise type ka value milta hai to ye us type ki value ko show karega otherwise popular ko show karega
+        fetch(`https://api.themoviedb.org/3/movie/ ${type ? type : "popular"} ? api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
         .then(res => res.json())
         .then(data => setMovieList(data.results))
     }
@@ -25,9 +29,11 @@ const MovieList = () => {
     return (
         <div className="movie__list">
             <h2 className="list__title">{(type ? type : "POPULAR").toUpperCase()}</h2>
+            
             <div className="list__cards">
                 {
                     movieList.map(movie => (
+//yha hame map se movie milega jise hamne card ke uder one by one put kr diya
                         <Cards movie={movie} />
                     ))
                 }
